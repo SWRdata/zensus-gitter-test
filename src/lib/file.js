e4665aee051985fa77c536880f430e1c38723a18
Michael Kreil <github@michael-kreil.de>
2024-02-29 14:57:00 +0100

    docs: file.js

--- src/lib/file.js
+++ src/lib/file.js
@@ -1,13 +1,18 @@
+// Import necessary modules from Node.js for filesystem and path manipulation
+import { readdirSync, statSync } from 'node:fs'; // Methods for reading directory contents and getting file stats
+import { resolve } from 'node:path'; // Method for resolving file paths
 
-import { readdirSync, statSync } from 'node:fs';
-import { resolve } from 'node:path';
-
+// Defines a function to get a list of files with a specific extension from a given directory
 export function getFiles(path) {
-	return readdirSync(path)
-		.filter(file => file.endsWith('.csv.br'))
-		.map(file => resolve(path, file));
+	return readdirSync(path) // Reads the contents of the directory
+		.filter(file => file.endsWith('.csv.br')) // Filters the files to only include ones with a '.csv.br' extension
+		.map(file => resolve(path, file)); // Resolves each file to an absolute path
 }
 
+// Defines a function to calculate the total size of a list of files
 export function getFileSum(files) {
-	return files.reduce((sum, file) => sum + statSync(file).size, 0)
+	return files.reduce((sum, file) =>
+		sum + statSync(file).size, // Adds the size of each file to the total sum
+		0 // Initializes the sum with 0
+	);
 }
